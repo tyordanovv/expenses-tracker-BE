0a45d005d8e579033cd8205647214e18c8c8bb2c
Tihomir Yordanov <t.yordanovv@gmail.com>
2023-04-16 22:55:31 +0300

    initial setup creation

---
+++ src/test/java/com/expenses/tracker/expensestracker/ExpensesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenses.tracker.expensestracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
